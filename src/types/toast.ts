7782183f7aeb3ceb03160f43665ff3f257bef4fc
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-12 17:56:18 +0100

    docs: updated Toast interface documentation
    - Added detailed JSDoc comments for `IToast` interface to explain its structure and methods.
    - Documented `add` method for adding toast messages, including an optional `duration` parameter.
    - Included description for `show` method to display the toast message.
    - Added documentation for `ToastOptions` type, specifying properties for `message` and `duration`.

--- src/types/toast.ts
+++ src/types/toast.ts
@@ -1,3 +1,34 @@
+/**
+ * The Toast interface represents a service for displaying temporary messages on the screen.
+ * @interface
+ */
+export interface IToast {
+  /**
+   * Array of toast messages.
+   */
+  toasts: ToastOptions[];
+  /**
+   * The timeout ID of the delayed function call.
+   */
+  timeout: number | null;
+  /**
+   * Adds a toast message to the service.
+   * @param {string} message - The text of the toast message.
+   * @param {number} [duration=30000] - The duration of the toast message in milliseconds.
+   */
+  add(message: string, duration: number): void;
+  /**
+   * Shows the toast message.
+   */
+  show(): void;
+}
+
+/**
+ * Options for the toast message.
+ * @typedef {Object} ToastOptions
+ * @property {string} message - The text of the toast message.
+ * @property {number} duration - The duration of the toast message in milliseconds.
+ */
 export type ToastOptions = {
   message: string;
   duration: number;
