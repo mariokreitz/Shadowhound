5f76a513bec3eadabdda704e2640ed1a6ce2894f
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-12 16:12:15 +0100

    feat: integrate `Player` class with initial game setup
    - Imported `Player` class and defined initial setup for integrating player rendering.
    - Added `Game` class implementing `IGame` with width, height, and player instance for structured game logic.
    - Initialized canvas context and dimensions (500x500) for game rendering.
    - Implemented `Game`'s `draw` method to render `Player` instance on the canvas, displaying initial game entity.
    - Added `update` placeholder in `Game` class for future state updates.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import "./styles/reset.css";
 import "./styles/toast.css";
 import "./style.css";
 import { Toast } from "./class/toast.class";
+import { IGame, CanvasDimensions } from "./types/shadowhound";
+import { Player } from "./class/player.class";
 
 window.addEventListener("load", function () {
   const canvasID = "canvas1";
@@ -11,5 +13,31 @@ window.addEventListener("load", function () {
     toast.add(`id: ${canvasID} not found`, 30000);
   }
 
-  const ctx = canvas.getContext("2d");
+  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+  canvas.width = 500;
+  canvas.height = 500;
+
+  class Game implements IGame {
+    width: number;
+    height: number;
+    player: Player;
+
+    constructor({ width, height }: CanvasDimensions) {
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this);
+    }
+
+    update() {
+      // Implementation for updating the game state
+    }
+
+    draw() {
+      // Implementation for drawing the game on the canvas
+      this.player.draw(ctx);
+    }
+  }
+
+  const game = new Game({ width: canvas.width, height: canvas.height });
+  game.draw();
 });
