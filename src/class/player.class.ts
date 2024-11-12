19cd63c21c5d518ffcaadfdddeda334243356673
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-12 18:00:20 +0100

    feat: update Player class with image handling
    - Added `showError` import from `utils/misc` for error handling.
    - Updated `Player` constructor to initialize `this.image` with the result of `getPlayerImage()`.
    - Implemented `getPlayerImage()` method to retrieve the player image from the DOM by ID, and show an error if the image is not found.

--- src/class/player.class.ts
+++ src/class/player.class.ts
@@ -1,4 +1,5 @@
 import { IGame, IPlayer } from "../types/shadowhound";
+import { showError } from "../utils/misc";
 
 export class Player implements IPlayer {
   game: IGame;
@@ -6,6 +7,7 @@ export class Player implements IPlayer {
   height: number;
   x: number;
   y: number;
+  image: HTMLImageElement;
 
   constructor(game: IGame) {
     this.game = game;
@@ -13,10 +15,24 @@ export class Player implements IPlayer {
     this.height = 91.3;
     this.x = 0;
     this.y = this.game.height - this.height;
+    this.image = this.getPlayerImage();
   }
+
   update() {}
 
   draw(ctx: CanvasRenderingContext2D): void {
     ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.x, this.y);
+  }
+
+  getPlayerImage(): HTMLImageElement {
+    const playerID = "player";
+    const playerImage = document.getElementById(playerID) as HTMLImageElement;
+    if (!playerImage) {
+      showError(playerID);
+      return new Image();
+    }
+
+    return playerImage;
   }
 }
