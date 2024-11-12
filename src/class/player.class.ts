aa6eb09bce4dfc8b28b062e5b429c3b4e1bffe41
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-12 19:24:27 +0100

    feat: implement horizontal and vertical movement in Player class
    - Implemented horizontal movement based on arrow keys (`ArrowRight` and `ArrowLeft`) with speed control.
    - Added constraints to prevent the player from moving off-screen.
    - Implemented vertical movement, applying gravity (vy) and jump control with `ArrowUp` key, only allowing jumping when the player is on the ground.
    - Included new properties `weight` and `vy` in the `Player` class to control gravity and vertical speed.

--- src/class/player.class.ts
+++ src/class/player.class.ts
@@ -8,6 +8,8 @@ export class Player implements IPlayer {
     this.height = 91.3;
     this.x = 0;
     this.y = this.game.height - this.height;
+    this.vy = 0;
+    this.weight = 1;
     this.image = this.getPlayerImage();
     this.speed = 0;
     this.maxSpeed = 10;
@@ -18,13 +20,24 @@ export class Player implements IPlayer {
   height: number;
   x: number;
   y: number;
+  weight: number;
+  vy: number;
   image: HTMLImageElement;
   speed: number;
   maxSpeed: number;
 
   update(input: string[]): void {
-    if (input.includes("ArrowRight")) this.x++;
-    else if (input.includes("ArrowLeft")) this.x--;
+    //horizontal movement
+    this.x += this.speed;
+    if (input.includes("ArrowRight")) this.speed = this.maxSpeed;
+    else if (input.includes("ArrowLeft")) this.speed = -this.maxSpeed;
+    else this.speed = 0;
+
+    if (this.x < 0) this.x = 0;
+    if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+    //vertical movement
+    this.y += this.vy;
+    if (input.includes("ArrowUp") && this.onGround()) this.vy -= 10;
   }
 
   draw(ctx: CanvasRenderingContext2D): void {
@@ -40,5 +53,9 @@ export class Player implements IPlayer {
     }
 
     return playerImage;
+  }
+
+  onGround(): boolean {
+    return this.y >= this.game.height - this.height;
   }
 }
