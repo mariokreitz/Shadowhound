914699eab916e78df3538343b93260dfa9ad326a
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-13 04:25:00 +0100

    feat: integrate Background into Game class and update rendering
    - Added `speed` property and `background` initialization in the `Game` class to align with the `IGame` interface.
    - Included `this.background.update()` and `this.background.draw(ctx)` in the game loop for background rendering and updates.
    - Ensured smooth integration of background layers with game updates.

--- src/class/game.class.ts
+++ src/class/game.class.ts
@@ -1,4 +1,5 @@
-import { IGame, IPlayer, IInputHandler, CanvasDimensions } from "./../types/shadowhound";
+import { IGame, IPlayer, IInputHandler, CanvasDimensions, IBackground } from "./../types/shadowhound";
+import { Background } from "./background.class";
 import { InputHandler } from "./input.class";
 import { Player } from "./player.class";
 
@@ -7,6 +8,8 @@ export class Game implements IGame {
     this.width = width;
     this.height = height;
     this.groundMargin = 50;
+    this.speed = 3;
+    this.background = new Background(this);
     this.player = new Player(this);
     this.input = new InputHandler();
   }
@@ -14,14 +17,18 @@ export class Game implements IGame {
   width: number;
   height: number;
   groundMargin: number;
+  speed: number;
+  background: IBackground;
   player: IPlayer;
   input: IInputHandler;
 
   update(deltaTime: number) {
+    this.background.update();
     this.player.update(this.input.keys, deltaTime);
   }
 
   draw(ctx: CanvasRenderingContext2D): void {
+    this.background.draw(ctx);
     this.player.draw(ctx);
   }
 }
