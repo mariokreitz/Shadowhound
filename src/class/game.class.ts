c119c5270d8a8e2bd88924f50d7d0f70bb5e5fc8
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-13 06:33:55 +0100

    feat: add enemy handling and drawing logic to game
    - Updated imports in the game file to include `IEnemy`, `FlyingEnemy`, `ClimbingEnemy`, and `GroundEnemy`.
    - Added `enemies` array, `enemyTimer`, and `enemyInterval` properties to manage enemy spawns and timing.
    - Set `enemies` to initialize as an empty array, with `enemyTimer` set to 0 and `enemyInterval` to 1000ms for controlled spawning.
    - Implemented enemy handling in the `update` method:
      - Checks if `enemyTimer` exceeds `enemyInterval`, then calls `addEnemy()` to spawn a new enemy and resets the timer.
      - Iterates over `enemies` array to update each enemy and removes any enemies marked for deletion.
    - Enhanced `draw` method to render each enemy alongside the background and player, providing a complete view of game elements.
    - Created `addEnemy` method to push a new `FlyingEnemy` instance to `enemies` and log the updated array, supporting dynamic game interaction and debugging.

--- src/class/game.class.ts
+++ src/class/game.class.ts
@@ -1,12 +1,14 @@
-import { IGame, IPlayer, IInputHandler, CanvasDimensions, IBackground } from "./../types/shadowhound";
+import { IGame, IPlayer, IInputHandler, CanvasDimensions, IBackground, IEnemy } from "./../types/shadowhound";
 import { Background } from "./background.class";
 import { InputHandler } from "./input.class";
 import { Player } from "./player.class";
+import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from "./enemies.class";
 
 export class Game implements IGame {
   private static readonly DEFAULT_GROUNDMARGIN = 80;
   private static readonly DEFAULT_SPEED = 0;
   private static readonly DEFAULT_MAX_SPEED = 3;
+  enemies: IEnemy[];
 
   constructor({ width, height }: CanvasDimensions) {
     this.width = width;
@@ -17,6 +19,9 @@ export class Game implements IGame {
     this.background = new Background(this);
     this.player = new Player(this);
     this.input = new InputHandler();
+    this.enemies = [];
+    this.enemyTimer = 0;
+    this.enemyInterval = 1000;
   }
 
   width: number;
@@ -24,6 +29,8 @@ export class Game implements IGame {
   groundMargin: number;
   speed: number;
   maxSpeed: number;
+  enemyTimer: number;
+  enemyInterval: number;
   background: IBackground;
   player: IPlayer;
   input: IInputHandler;
@@ -31,10 +38,28 @@ export class Game implements IGame {
   update(deltaTime: number) {
     this.background.update();
     this.player.update(this.input.keys, deltaTime);
+    // handle enemies
+    if (this.enemyTimer > this.enemyInterval) {
+      this.addEnemy();
+      this.enemyTimer = 0;
+    } else this.enemyTimer += deltaTime;
+
+    this.enemies.forEach((enemy) => {
+      enemy.update(deltaTime);
+      if (enemy.markedForDeletion) this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion);
+    });
   }
 
   draw(ctx: CanvasRenderingContext2D): void {
     this.background.draw(ctx);
     this.player.draw(ctx);
+    this.enemies.forEach((enemy) => {
+      enemy.draw(ctx);
+    });
+  }
+
+  addEnemy() {
+    this.enemies.push(new FlyingEnemy(this));
+    console.log(this.enemies);
   }
 }
