52fef23bfd6a3db6e35fb1b1a4cb48e7c1a8c804
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-13 06:34:42 +0100

    feat: extend enemy classes with image handling and dynamic spawning properties
    - Updated imports in `enemies.class.ts` to include `IFlyingEnemy` and `IGame` interfaces along with `getImage` utility.
    - Modified `Enemy` base class:
      - Added `game` parameter to the constructor to integrate game-wide properties.
      - Defined properties for dimensions (`width`, `heigh`), position (`x`, `y`), image (`image`), speed (`speedX`, `speedY`), animation frames (`frameX`, `maxFrame`), and deletion status (`markedForDeletion`).
      - Updated `update` method to manage horizontal movement, frame updates based on `frameInterval`, and set deletion if the enemy moves off-screen.
      - Enhanced `draw` method to display the correct animation frame using `drawImage`.
    - Refined `FlyingEnemy` class with specific attributes:
      - Set initial properties for dimensions, random positioning, speed, animation frame count, and image loading through `getImage`.
      - Introduced `angle` and `va` properties for potential oscillatory movement.
      - Added custom `update` method that builds on the `Enemy` class’s `update`.
    - Added `GroundEnemy` and `ClimbingEnemy` classes as extensions of `Enemy`, allowing for future customization of ground and climbing enemy types.

--- src/class/enemies.class.ts
+++ src/class/enemies.class.ts
@@ -1,26 +1,91 @@
-import { IEnemy } from "../types/shadowhound";
+import { IEnemy, IFlyingEnemy, IGame } from "../types/shadowhound";
+import { getImage } from "../utils/misc";
 
 class Enemy implements IEnemy {
-  constructor() {
+  constructor(game: IGame) {
+    this.game = game;
+    this.width = 0;
+    this.heigh = 0;
+    this.x = 0;
+    this.y = 0;
+    this.image = new Image();
+    this.speedX = 0;
+    this.speedY = 0;
+    this.maxFrame = 0;
     this.frameX = 0;
     this.frameY = 0;
     this.fps = 20;
     this.frameInterval = 1000 / this.fps;
     this.frameTimer = 0;
+    this.markedForDeletion = false;
   }
-
+  game: IGame;
+  heigh: number;
+  width: number;
+  x: number;
+  y: number;
+  image: HTMLImageElement;
+  speedX: number;
+  speedY: number;
+  maxFrame: number;
   frameX: number;
   frameY: number;
   fps: number;
   frameInterval: number;
   frameTimer: number;
+  markedForDeletion: boolean;
 
-  update(deltaTime: number): void {}
-  draw(ctx: CanvasRenderingContext2D): void {}
+  update(deltaTime: number): void {
+    this.x -= this.speedX + this.game.speed;
+    this.y += this.speedY;
+    if (this.frameTimer > this.frameInterval) {
+      this.frameTimer = 0;
+      if (this.frameX < this.maxFrame) this.frameX++;
+      else this.frameX = 0;
+    } else this.frameTimer += deltaTime;
+
+    // check if off screen
+    if (this.x + this.width < 0) this.markedForDeletion = true;
+  }
+
+  draw(ctx: CanvasRenderingContext2D): void {
+    ctx.drawImage(
+      this.image,
+      this.frameX * this.width,
+      0,
+      this.width,
+      this.heigh,
+      this.x,
+      this.y,
+      this.width,
+      this.heigh
+    );
+  }
 }
 
-class FlyingEnemy extends Enemy {}
+export class FlyingEnemy extends Enemy implements IFlyingEnemy {
+  constructor(game: IGame) {
+    super(game);
+    this.width = 60;
+    this.heigh = 44;
+    this.x = this.game.width + Math.random() * this.game.width * 0.5;
+    this.y = Math.random() * this.game.height * 0.5;
+    this.speedX = Math.random() + 1;
+    this.speedY = 0;
+    this.maxFrame = 5;
+    this.image = getImage("enemy_fly");
+    this.angle = 0;
+    this.va = Math.random() * 0.1 + 0.1;
+  }
 
-class GroundEnemy extends Enemy {}
+  angle: number;
+  va: number;
 
-class ClimbingEnemy extends Enemy {}
+  update(deltaTime: number): void {
+    super.update(deltaTime);
+  }
+}
+
+export class GroundEnemy extends Enemy {}
+
+export class ClimbingEnemy extends Enemy {}
