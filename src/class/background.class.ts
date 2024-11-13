a1d03e572b3fa9009da764542c35f491d6eb6f6b
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-13 04:47:36 +0100

    feat: add multiple background layers for endless scrolling
    - Implemented multiple background layers (layer1 to layer4) to enhance the visual depth and scrolling effect.
    - Updated the `drawImage` method to draw the image twice, creating an endless loop effect for smooth scrolling.
    - Set up layer speeds with different modifiers to achieve parallax scrolling effect.

--- src/class/background.class.ts
+++ src/class/background.class.ts
@@ -22,6 +22,7 @@ class Layer implements ILayer {
 
   draw(ctx: CanvasRenderingContext2D): void {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
   }
 
   update(): void {
@@ -35,22 +36,62 @@ export class Background implements IBackground {
     this.game = game;
     this.width = 1667;
     this.height = 500;
+    this.layer1Image = getImage("layer-1");
+    this.layer2Image = getImage("layer-2");
+    this.layer3Image = getImage("layer-3");
+    this.layer4Image = getImage("layer-4");
     this.layer5Image = getImage("layer-5");
     this.layer1 = new Layer({
+      game: this.game,
+      width: this.width,
+      height: this.height,
+      speedModifier: 0,
+      image: this.layer1Image,
+    });
+    this.layer2 = new Layer({
+      game: this.game,
+      width: this.width,
+      height: this.height,
+      speedModifier: 0.2,
+      image: this.layer2Image,
+    });
+    this.layer3 = new Layer({
+      game: this.game,
+      width: this.width,
+      height: this.height,
+      speedModifier: 0.4,
+      image: this.layer3Image,
+    });
+    this.layer4 = new Layer({
+      game: this.game,
+      width: this.width,
+      height: this.height,
+      speedModifier: 0.8,
+      image: this.layer4Image,
+    });
+    this.layer5 = new Layer({
       game: this.game,
       width: this.width,
       height: this.height,
       speedModifier: 1,
       image: this.layer5Image,
     });
-    this.backgroundLayers = [this.layer1];
+    this.backgroundLayers = [this.layer1, this.layer2, this.layer3, this.layer4, this.layer5];
   }
 
   game: IGame;
   width: number;
   height: number;
+  layer1Image: HTMLImageElement;
+  layer2Image: HTMLImageElement;
+  layer3Image: HTMLImageElement;
+  layer4Image: HTMLImageElement;
   layer5Image: HTMLImageElement;
   layer1: ILayer;
+  layer2: ILayer;
+  layer3: ILayer;
+  layer4: ILayer;
+  layer5: ILayer;
   backgroundLayers: ILayer[];
 
   draw(ctx: CanvasRenderingContext2D): void {
