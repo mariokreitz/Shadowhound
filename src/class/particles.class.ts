b05a33050e9063747f5f521a4bc952383bab40cd
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-14 05:01:43 +0100

    feat: add Splash particle effect with gravity
    - Created a new `Splash` class extending `Particle` with unique behavior and gravity.
    - Introduced random speeds for `Splash` particles and a custom gravity effect.
    - Used the `fire` image for the splash effect, providing a visual representation of the particle.

--- src/class/particles.class.ts
+++ src/class/particles.class.ts
@@ -49,7 +49,31 @@ export class Dust extends Particle {
   }
 }
 
-export class Splash extends Particle {}
+export class Splash extends Particle {
+  constructor(game: IGame, x: number, y: number) {
+    super(game);
+    this.size = Math.random() * 100 + 100;
+    this.x = x - this.size * 0.4;
+    this.y = y - this.size * 0.5;
+    this.speedX = Math.random() * 6 - 4;
+    this.speedY = Math.random() * 2 + 1;
+    this.gravity = 0;
+    this.image = getImage("fire");
+  }
+
+  gravity: number;
+  image: HTMLImageElement;
+
+  update() {
+    super.update();
+    this.gravity += 0.1;
+    this.y += this.gravity;
+  }
+
+  draw(ctx: CanvasRenderingContext2D): void {
+    ctx.drawImage(this.image, this.x, this.y, this.size, this.size);
+  }
+}
 
 export class Fire extends Particle {
   constructor(game: IGame, x: number, y: number) {
