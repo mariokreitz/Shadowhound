43ceed8ea2c365852c36ea2921116fa262e6925d
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-14 03:06:39 +0100

    feat: add ROLLING state and Rolling class for player actions
    - Expanded `states` enum to include `ROLLING`, `DIVING`, and `HIT` states.
    - Created `Rolling` class extending `State`, implementing the rolling mechanic for the player.
      - `enter()` sets initial animation frames for rolling.
      - `handleInput()` transitions to `RUNNING` or `FALLING` states when `Enter` is released, or to `JUMPING` if `Enter` and `ArrowUp` are pressed simultaneously.
    - Integrated functionality to allow player rolling from any state.

--- src/class/playerState.class.ts
+++ src/class/playerState.class.ts
@@ -5,6 +5,9 @@ enum states {
   RUNNING = 1,
   JUMPING = 2,
   FALLING = 3,
+  ROLLING = 4,
+  DIVING = 5,
+  HIT = 6,
 }
 
 class State implements IState {
@@ -29,6 +32,7 @@ export class Sitting extends State implements IStateAction {
 
   handleInput(input: string[]) {
     if (input.includes("ArrowLeft") || input.includes("ArrowRight")) this.player.setState(states.RUNNING, 1);
+    else if (input.includes("Enter")) this.player.setState(states.ROLLING, 2);
   }
 }
 
@@ -48,6 +52,7 @@ export class Running extends State implements IStateAction {
   handleInput(input: string[]) {
     if (input.includes("ArrowDown")) this.player.setState(states.SITTING, 0);
     else if (input.includes("ArrowUp")) this.player.setState(states.JUMPING, 1);
+    else if (input.includes("Enter")) this.player.setState(states.ROLLING, 2);
   }
 }
 
@@ -65,8 +70,9 @@ export class Jumping extends State implements IStateAction {
     this.player.frameY = 1;
   }
 
-  handleInput() {
+  handleInput(input: string[]) {
     if (this.player.vy > this.player.weight) this.player.setState(states.FALLING, 1);
+    else if (input.includes("Enter")) this.player.setState(states.ROLLING, 2);
   }
 }
 
@@ -85,5 +91,26 @@ export class Falling extends State implements IStateAction {
 
   handleInput() {
     if (this.player.onGround()) this.player.setState(states.RUNNING, 1);
+  }
+}
+
+export class Rolling extends State implements IStateAction {
+  player: IPlayer;
+  constructor(player: IPlayer) {
+    super("ROLLING");
+    this.player = player;
+  }
+
+  enter() {
+    this.player.frameX = 0;
+    this.player.maxFrame = 6;
+    this.player.frameY = 6;
+  }
+
+  handleInput(input: string[]) {
+    if (!input.includes("Enter") && this.player.onGround()) this.player.setState(states.RUNNING, 1);
+    else if (!input.includes("Enter") && !this.player.onGround()) this.player.setState(states.FALLING, 1);
+    else if (input.includes("Enter") && input.includes("ArrowUp") && this.player.onGround())
+      this.player.setState(states.JUMPING, 1);
   }
 }
