0ab5e9bb688d33e8d3567aa5ffe80c0afe579c97
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-14 06:05:33 +0100

    feat: enhance UI with timer and game-over messages
    - Added timer display to show elapsed time.
    - Updated game-over screen with different messages based on score.
    - Improved text visibility with a shadow effect for UI elements.

--- src/class/UI.class.ts
+++ src/class/UI.class.ts
@@ -12,10 +12,37 @@ export class UI {
   fontFamily: string;
 
   draw(ctx: CanvasRenderingContext2D) {
+    ctx.save();
+    ctx.shadowOffsetX = 2;
+    ctx.shadowOffsetY = 2;
+    ctx.shadowColor = "white";
+    ctx.shadowBlur = 0;
     ctx.font = `${this.fontSize}px ${this.fontFamily}`;
     ctx.textAlign = "left";
     ctx.fillStyle = this.game.fontColor;
     //score
     ctx.fillText(`Score: ${this.game.score}`, 20, 50);
+    //timer
+    ctx.font = `${this.fontSize * 0.8}px ${this.fontFamily}`;
+    ctx.fillText(`Time: ${(this.game.time * 0.001).toFixed(1)}`, 20, 80);
+    //game over messages
+    if (this.game.gameOver) {
+      ctx.textAlign = "center";
+      ctx.font = `${this.fontSize * 2}px ${this.fontFamily}`;
+      if (this.game.score > this.game.minScore) {
+        ctx.fillText("Boo-yah", this.game.width * 0.5, this.game.height * 0.5 - 20);
+        ctx.font = `${this.fontSize * 0.7}px ${this.fontFamily}`;
+        ctx.fillText(
+          "What are creatures of the night afraid of? YOU!!",
+          this.game.width * 0.5,
+          this.game.height * 0.5 + 20
+        );
+      } else {
+        ctx.fillText("Love at first bite?", this.game.width * 0.5, this.game.height * 0.5 - 20);
+        ctx.font = `${this.fontSize * 0.7}px ${this.fontFamily}`;
+        ctx.fillText("Nope, better luck next time!", this.game.width * 0.5, this.game.height * 0.5 + 20);
+      }
+    }
+    ctx.restore();
   }
 }
