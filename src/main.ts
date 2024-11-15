1d2042166f34e3cb74462bd29d54785df93e44f5
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-15 22:07:54 +0100

    feat: integrate main menu functionality and game start logic
    - Updated imports to include `getMenuElements` for menu interactions.
    - Added logic to toggle the main menu display using `toggleMenu`.
    - Implemented retrieval of canvas and context with error handling for undefined elements.
    - Enhanced button functionality:
      - `startBtn`: Starts the game and toggles the main menu off, adding a border to the canvas.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
-import "./styles/reset.css";
 import "./styles/font.css";
+import "./styles/reset.css";
 import "./styles/toast.css";
 import "./style.css";
-import { toggleMenu, hideLoadingScreen, getCanvasAndContext } from "./utils/misc";
+import { toggleMenu, hideLoadingScreen, getCanvasAndContext, getMenuElements } from "./utils/misc";
 import { Game } from "./class/game.class";
 
 window.addEventListener("load", function () {
   hideLoadingScreen("loading");
-  toggleMenu("mainMenu");
-  const { canvas, ctx } = getCanvasAndContext("canvas1") || { canvas: undefined, ctx: undefined };
+  document.getElementById("main-menu")?.style.removeProperty("display");
+
+  const { canvas, ctx } = getCanvasAndContext("canvas1") || {};
   if (!canvas || !ctx) return;
+
+  const { startBtn, helpBtn } = getMenuElements() || {};
+  if (!startBtn || !helpBtn) return;
 
   canvas.width = 900;
   canvas.height = 500;
 
   const game = new Game(canvas.width, canvas.height, ctx);
-  game.start();
+
+  startBtn.addEventListener("click", () => {
+    game.start();
+    toggleMenu("main-menu");
+    canvas.style.border = "5px solid black";
+  });
 });
