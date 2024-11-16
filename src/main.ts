2efca79a78fafacb3b6c79ed824ee98c426cae23
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-16 07:07:17 +0100

    feat: Add volume control functionality and button interactions
    - Implemented event listeners for volume control and sound control buttons.
    - Added toggle functionality for game and menu music volumes with visual feedback on button states.
    - Integrated hover and click sound effects for menu buttons with disabled state handling.

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,75 @@ import "./styles/font.css";
 import "./styles/reset.css";
 import "./styles/toast.css";
 import "./style.css";
-import { toggleMenu, hideLoadingScreen, getCanvasAndContext, getMenuElements } from "./utils/misc";
+import {
+  toggleMenu,
+  hideLoadingScreen,
+  getCanvasAndContext,
+  getMenuElements,
+  addVolumeControl,
+  toggleVolume,
+} from "./utils/misc";
 import { Game } from "./class/game.class";
+import { soundIcons, volumeIcons } from "./utils/svgIcons";
 
 window.addEventListener("load", function () {
   hideLoadingScreen("loading");
+  addVolumeControl("controls");
   document.getElementById("main-menu")?.style.removeProperty("display");
 
   const { canvas, ctx } = getCanvasAndContext("canvas1") || {};
   if (!canvas || !ctx) return;
 
-  const { startBtn, helpBtn } = getMenuElements() || {};
-  if (!startBtn || !helpBtn) return;
+  const { startButton, helpButton, soundControlButton, volumeControlButton } = getMenuElements() || {};
+  if (!startButton || !helpButton || !soundControlButton || !volumeControlButton) return;
 
   canvas.width = 900;
   canvas.height = 500;
 
   const game = new Game(canvas.width, canvas.height, ctx);
+  game.menuMusic.start();
 
-  startBtn.addEventListener("click", () => {
+  startButton.addEventListener("click", () => {
     game.start();
     toggleMenu("main-menu");
     canvas.style.border = "5px solid black";
+  });
+
+  volumeControlButton.addEventListener("click", () => {
+    toggleVolume(game.menuClickEffect);
+    toggleVolume(game.menuHoverEffect);
+    if (game.menuClickEffect.currentVolumeState == 1) volumeControlButton.innerHTML = volumeIcons.low;
+    else if (game.menuClickEffect.currentVolumeState == 2) volumeControlButton.innerHTML = volumeIcons.off;
+    else volumeControlButton.innerHTML = volumeIcons.high;
+  });
+
+  soundControlButton.addEventListener("click", () => {
+    toggleVolume(game.gameMusic);
+    toggleVolume(game.menuMusic);
+
+    if (game.menuMusic.currentVolumeState == 2) soundControlButton.innerHTML = soundIcons.off;
+    else soundControlButton.innerHTML = soundIcons.on;
+  });
+
+  [startButton, helpButton, soundControlButton, volumeControlButton].forEach((button) => {
+    if (button.id !== "sound-control" && button.id !== "volume-control") {
+      button.addEventListener("mouseover", () => {
+        if (button.disabled) return;
+        game.menuHoverEffect.start();
+      });
+    }
+    button.addEventListener("click", () => {
+      if (button.disabled) return;
+      button.disabled = true;
+      game.menuClickEffect.start();
+      game.menuClickEffect.audioFile.addEventListener(
+        "ended",
+        () => {
+          button.disabled = false;
+        },
+        { once: true }
+      );
+      game.menuClickEffect.start();
+    });
   });
 });
