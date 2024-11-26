bc29384e76e36caac787b54225f2e9fd108489a0
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-26 13:28:45 +0100

    docs: add JSDocs to collectables.class.ts
    - Added JSDoc comments to all methods and properties in the `collectables.class.ts`
    - Provided detailed descriptions for each class function
    - Improved code readability and documentation for easier collaboration

--- src/class/collectables.class.ts
+++ src/class/collectables.class.ts
@@ -1,7 +1,37 @@
 import { ICollectable, IGame } from "../types/shadowhound";
 import { getImage } from "../utils/misc";
 
+/**
+ * A collectable item in the game.
+ *
+ * @class Collectable
+ * @implements {ICollectable}
+ * @property {IGame} game - The game object.
+ * @property {number} x - The x position of the collectable.
+ * @property {number} y - The y position of the collectable.
+ * @property {number} width - The width of the collectable.
+ * @property {number} height - The height of the collectable.
+ * @property {HTMLImageElement} image - The image element for the collectable.
+ * @property {number} speedX - The x speed of the collectable.
+ * @property {number} speedY - The y speed of the collectable.
+ */
 class Collectable implements ICollectable {
+  /**
+   * Constructs an instance of the Collectable class.
+   *
+   * @param {IGame} game - The game object.
+   * @property {IGame} game - The game object.
+   * @property {number} x - The initial x position of the collectable.
+   * @property {number} y - The initial y position of the collectable.
+   * @property {number} width - The initial width of the collectable.
+   * @property {number} height - The initial height of the collectable.
+   * @property {HTMLImageElement} image - The image element for the collectable.
+   * @property {number} speedX - The x speed of the collectable.
+   * @property {number} speedY - The y speed of the collectable.
+   * @property {number} frameX - The x position of the frame.
+   * @property {number} frameY - The y position of the frame.
+   * @property {boolean} markedForDeletion - Indicates if the collectable is marked for deletion.
+   */
   constructor(game: IGame) {
     this.game = game;
     this.x = 0;
@@ -28,11 +58,27 @@ class Collectable implements ICollectable {
   frameY: number;
   markedForDeletion: boolean;
 
+  /**
+   * Draws the collectable on the canvas.
+   *
+   * @param {CanvasRenderingContext2D} ctx - The canvas context.
+   * @remarks
+   * - If the game is in debug mode, a rectangle is drawn around the collectable's bounding box.
+   * - The collectable image is drawn on the canvas at its current position.
+   */
   draw(ctx: CanvasRenderingContext2D) {
     if (this.game.debug) ctx.strokeRect(this.x, this.y, this.width, this.height);
     ctx.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
   }
 
+  /**
+   * Updates the position of the collectable.
+   *
+   * @remarks
+   * - Decreases the x position by the sum of speedX and the game's speed.
+   * - Increases the y position by speedY.
+   * - Marks the collectable for deletion if it moves out of the screen on the left.
+   */
   update() {
     this.x -= this.speedX + this.game.speed;
     this.y += this.speedY;
@@ -40,7 +86,29 @@ class Collectable implements ICollectable {
   }
 }
 
+/**
+ * DogHead class, a type of collectable in the game.
+ *
+ * @class DogHead
+ * @extends Collectable
+ * @param {IGame} game - The game object.
+ * @property {number} width - The width of the DogHead.
+ * @property {number} height - The height of the DogHead.
+ * @property {number} x - The x position of the DogHead.
+ * @property {number} y - The y position of the DogHead.
+ * @property {HTMLImageElement} image - The image element for the DogHead.
+ */
 export class DogHead extends Collectable {
+  /**
+   * Constructs an instance of the DogHead class.
+   *
+   * @param {IGame} game - The game object.
+   * @property {number} width - The width of the DogHead.
+   * @property {number} height - The height of the DogHead.
+   * @property {number} x - The x position of the DogHead.
+   * @property {number} y - The y position of the DogHead.
+   * @property {HTMLImageElement} image - The image element for the DogHead.
+   */
   constructor(game: IGame) {
     super(game);
     this.width = 50;
@@ -51,7 +119,29 @@ export class DogHead extends Collectable {
   }
 }
 
+/**
+ * Heart class, a type of collectable in the game.
+ *
+ * @class Heart
+ * @extends Collectable
+ * @param {IGame} game - The game object.
+ * @property {number} width - The width of the Heart.
+ * @property {number} height - The height of the Heart.
+ * @property {number} x - The x position of the Heart.
+ * @property {number} y - The y position of the Heart.
+ * @property {HTMLImageElement} image - The image element for the Heart.
+ */
 export class Heart extends Collectable {
+  /**
+   * Constructs an instance of the Heart class.
+   *
+   * @param {IGame} game - The game object.
+   * @property {number} width - The width of the Heart.
+   * @property {number} height - The height of the Heart.
+   * @property {number} x - The x position of the Heart.
+   * @property {number} y - The y position of the Heart.
+   * @property {HTMLImageElement} image - The image element for the Heart.
+   */
   constructor(game: IGame) {
     super(game);
     this.width = 50;
@@ -59,5 +149,18 @@ export class Heart extends Collectable {
     this.x = this.game.width;
     this.y = this.game.height - this.height - this.game.groundMargin;
     this.image = getImage("lives");
+  }
+
+  /**
+   * Draws the Heart collectable on the canvas.
+   *
+   * @param {CanvasRenderingContext2D} ctx - The canvas context.
+   * @remarks
+   * - If the game is in debug mode, a rectangle is drawn around the Heart's bounding box.
+   * - The Heart image is drawn on the canvas at its current position.
+   */
+  draw(ctx: CanvasRenderingContext2D) {
+    if (this.game.debug) ctx.strokeRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y + 15, 35, 35);
   }
 }
