4a3ca61e688d8389d9e0a1c6c475cca94da43c3e
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-26 12:56:36 +0100

    feat: enhance collectables system with new functionality
    - Updated `Collectable` class to improve its base functionality and flexibility.
    - Refined the `DogHead` class to align with the updated collectables structure.
    - Added a new `Heart` class to allow players to refill lost lives upon collection.
    - Integrated the updated collectables into the game logic for dynamic gameplay enhancements.

--- src/class/collectables.class.ts
+++ src/class/collectables.class.ts
@@ -9,6 +9,10 @@ class Collectable implements ICollectable {
     this.width = 0;
     this.height = 0;
     this.image = new Image();
+    this.speedX = 0;
+    this.speedY = 0;
+    this.frameX = 0;
+    this.frameY = 0;
     this.markedForDeletion = false;
   }
 
@@ -18,17 +22,42 @@ class Collectable implements ICollectable {
   width: number;
   height: number;
   image: HTMLImageElement;
+  speedX: number;
+  speedY: number;
+  frameX: number;
+  frameY: number;
   markedForDeletion: boolean;
 
-  draw(ctx: CanvasRenderingContext2D) {}
+  draw(ctx: CanvasRenderingContext2D) {
+    if (this.game.debug) ctx.strokeRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+  }
 
-  update() {}
+  update() {
+    this.x -= this.speedX + this.game.speed;
+    this.y += this.speedY;
+    if (this.x + this.width < 0) this.markedForDeletion = true;
+  }
 }
 
 export class DogHead extends Collectable {
   constructor(game: IGame) {
     super(game);
-
+    this.width = 50;
+    this.height = 50;
+    this.x = this.game.width;
+    this.y = this.game.height - this.height - this.game.groundMargin;
     this.image = getImage("player-head");
+  }
+}
+
+export class Heart extends Collectable {
+  constructor(game: IGame) {
+    super(game);
+    this.width = 50;
+    this.height = 50;
+    this.x = this.game.width;
+    this.y = this.game.height - this.height - this.game.groundMargin;
+    this.image = getImage("lives");
   }
 }
